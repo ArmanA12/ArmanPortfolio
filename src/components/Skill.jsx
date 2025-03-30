f7b6e1221049168ab3e284eb0e09bbcbeaa6e14a
Arman shekh <mdarmanshekh4@gmail.com>
2025-03-30 16:17:18 +0530

    adding Images

--- src/components/Skill.jsx
+++ src/components/Skill.jsx
@@ -3,77 +3,63 @@ import { Cog } from 'lucide-react';
 
 
 function Skill() {
-  return (
-    <div className='Skillmain'>
-        <div style={{display:"flex",justifyContent:"center", alignItems:"center", gap:"15px"}}>
-        <Cog size={50} strokeWidth={0.5} />
-        <h3 style={{fontSize:"30px", letterSpacing:"3px",fontWeight:"400"}}> Technical Skill </h3>    
+    return (
+        <div className='Skillmain'>
+            <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "15px" }}>
+                <Cog size={50} strokeWidth={0.5} />
+                <h3 style={{ fontSize: "30px", letterSpacing: "3px", fontWeight: "400" }}> Technical Skill </h3>
+            </div>
+
+            <div className='skillContainer'>
+                <div className="skill-card">
+                    <img loading="lazy" src="../html.png" alt="HTML" />
+                    <div className="stick stick-red"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../css.png" alt="CSS" />
+                    <div className="stick stick-blue"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../reactjs.png" alt="React" />
+                    <div className="stick stick-blue"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../typescript.png" alt="TypeScript" />
+                    <div className="stick stick-blue"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../javascript.png" alt="JavaScript" />
+                    <div className="stick stick-yellow"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../nodejs.png" alt="Node.js" />
+                    <div className="stick stick-green"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../mongodb.png" alt="MongoDB" />
+                    <div className="stick stick-green"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../next.png" alt="Next.js" />
+                    <div className="stick stick-white"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../docker.png" alt="Docker" />
+                    <div className="stick stick-blue"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../git.png" alt="Git" />
+                    <div className="stick stick-red"></div>
+                </div>
+                <div className="skill-card">
+                    <img loading="lazy" src="../redux.png" alt="Redux" />
+                    <div className="stick stick-purple"></div>
+                </div>
+
+
+            </div>
         </div>
-
-        <div className='skillContainer'>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../html.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../css.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../reactjs.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../javascript.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../typescript.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../tailwind.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../nodejs.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../mongodb.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../docker.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../next.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img  loadind="lazy" src="../git.png" alt="img" />
-                <div className='stick'></div>
-
-            </div>
-            <div className='skill-card'>
-                <img loadind="lazy" src="../redux.png" alt="img" />
-                <div className='stick'></div>
-            </div>
-
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Skill
